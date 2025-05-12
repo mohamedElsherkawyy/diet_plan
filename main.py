0aec6e569dba630205d454b2f9e02039a9f4bc93
mohamedElsherkawyy <mohamedmotaz942@gmail.com>
2025-05-12 23:33:42 +0300

    Implement initial project structure and add essential files

---
+++ main.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel,conlist
+from typing import List,Optional
+import pandas as pd
+from model import recommend,output_recommended_recipes
+import uvicorn
+import pandas as pd
+
+
+print("Loading dataset...")
+
+try:
+    dataset=pd.read_csv('dataset.csv' , compression='gzip',encoding='ISO-8859-1')
+    print("✅ Dataset loaded successfully!")
+except FileNotFoundError:
+    print("❌ Dataset file not found. Check the path and file name.")
+    dataset = None
+except Exception as e:
+    print("❌ Failed to load dataset:", str(e))
+    dataset = None
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class params(BaseModel):
+    n_neighbors:int=5
+    return_distance:bool=False
+
+class PredictionIn(BaseModel):
+    nutrition_input: list[float]
+    ingredients: list[str] = []
+    params: Optional[params]
+
+
+class Recipe(BaseModel):
+    Name:str
+    CookTime:int
+    PrepTime:int
+    TotalTime:int
+    RecipeIngredientParts:list[str]
+    Calories:float
+    FatContent:float
+    SaturatedFatContent:float
+    CholesterolContent:float
+    SodiumContent:float
+    CarbohydrateContent:float
+    FiberContent:float
+    SugarContent:float
+    ProteinContent:float
+    RecipeInstructions:list[str]
+
+class PredictionOut(BaseModel):
+    output: Optional[List[Recipe]] = None
+
+
+@app.get("/")
+def home():
+    return {"health_check": "OK"}
+
+
+@app.post("/predict",response_model=PredictionOut)
+def update_item(prediction_input:PredictionIn):
+    print(prediction_input)
+    recommendation_dataframe=recommend(dataset,prediction_input.nutrition_input,prediction_input.ingredients,prediction_input.params.dict())
+    print(recommendation_dataframe)
+    output=output_recommended_recipes(recommendation_dataframe)
+    if output is None:
+        return {"output":None}
+    else:
+        return {"output":output}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
---
+++ model.py
@@ -0,0 +1,69 @@
+import numpy as np
+import re
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+
+
+def scaling(dataframe):
+    scaler=StandardScaler()
+    prep_data=scaler.fit_transform(dataframe.iloc[:,6:15].to_numpy())
+    return prep_data,scaler
+
+def nn_predictor(prep_data):
+    neigh = NearestNeighbors(metric='cosine',algorithm='brute')
+    neigh.fit(prep_data)
+    return neigh
+
+def build_pipeline(neigh,scaler,params):
+    transformer = FunctionTransformer(neigh.kneighbors,kw_args=params)
+    pipeline=Pipeline([('std_scaler',scaler),('NN',transformer)])
+    return pipeline
+
+def extract_data(dataframe,ingredients):
+    extracted_data=dataframe.copy()
+    extracted_data=extract_ingredient_filtered_data(extracted_data,ingredients)
+    return extracted_data
+    
+def extract_ingredient_filtered_data(dataframe, ingredients):
+    ingredients = set(map(lambda x: x.lower().strip(), ingredients))
+    def has_ingredients(parts):
+        parts_set = set(map(lambda x: x.lower().strip(), extract_quoted_strings(parts)))
+        return ingredients.issubset(parts_set)
+    extracted_data = dataframe[dataframe['RecipeIngredientParts'].apply(has_ingredients)]
+    return extracted_data
+
+def apply_pipeline(pipeline,_input,extracted_data):
+    _input=np.array(_input).reshape(1,-1)
+    return extracted_data.iloc[pipeline.transform(_input)[0]]
+
+def recommend(dataframe,_input,ingredients=[],params={'n_neighbors':5,'return_distance':False}):
+        extracted_data=extract_data(dataframe,ingredients)
+        if extracted_data.shape[0]>=params['n_neighbors']:
+            prep_data,scaler=scaling(extracted_data)
+            neigh=nn_predictor(prep_data)
+            pipeline=build_pipeline(neigh,scaler,params)
+            return apply_pipeline(pipeline,_input,extracted_data)
+        else:
+            return None
+
+def extract_quoted_strings(s):
+    s = s.strip()
+    if s.startswith('c(') and s.endswith(')'):
+        s = s[2:-1]
+    strings = re.findall(r'"((?:[^"\\]|\\.)*)"', s)
+    strings = [x.replace('\\"', '"').strip() for x in strings if x.strip()]
+    return strings
+
+def output_recommended_recipes(dataframe):
+    if dataframe is not None:
+        output=dataframe.copy()
+        output=output.to_dict("records")
+        for recipe in output:
+            recipe['RecipeIngredientParts']=extract_quoted_strings(recipe['RecipeIngredientParts'])
+            recipe['RecipeInstructions']=extract_quoted_strings(recipe['RecipeInstructions'])
+    else:
+        output=None
+    return output
+
